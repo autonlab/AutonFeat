c90417372afa809312054d5ff8284feba382e381
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-05-11 16:11:24 -0400

    Add IQR featurizer

---
+++ autofeat/common/iqr_transform.py
@@ -0,0 +1,29 @@
+import numpy as np
+from typing import Union
+
+from autofeat.core import Transform
+from autofeat.functional import iqr_tf
+
+class IQRTransform(Transform):
+    """
+    Compute the inter-quartile range of the values.
+
+    Inherits from Transform.
+    """
+    # Dunder methods
+    def __init__(self):
+        super().__init__(name="IQR")
+    
+    def __call__(self, signal_window: np.ndarray, method: str='linear') -> Union[np.float_, np.int_]:
+        """
+        Compute the inter-quartile range of the values in `x`.
+
+        Args:
+            `x`: The array to compute the IQR of.
+            
+            `method`: The method to use when computing the quantiles. Default is 'linear'. See `numpy.quantile` for more information.
+        
+        Returns:
+            A scalar value representing the IQR of the signal.
+        """
+        return iqr_tf(signal_window, method=method)
---
+++ autofeat/functional/iqr.py
@@ -0,0 +1,19 @@
+import numpy as np
+from typing import Union
+
+from autofeat.functional import quantile_tf
+
+def iqr_tf(x: np.ndarray, method: str='linear') -> Union[float, np.float_]:
+    """
+    Compute the inter-quartile range of the values in `x`.
+
+    Args:
+        `x`: The array to compute the IQR of.
+        
+        `method`: The method to use when computing the quantiles. Default is 'linear'. See `numpy.quantile` for more information.
+
+    Returns:
+        The IQR of the values in `x`.
+    """
+
+    return quantile_tf(x, q=0.75, method=method) - quantile_tf(x, q=0.25, method=method)
