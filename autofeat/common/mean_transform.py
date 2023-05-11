9e1bf81d69c985ce5df5059e8daaeb75ab902c29
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-05-11 14:45:33 -0400

    Update mean transform docs

--- autofeat/common/mean_transform.py
+++ autofeat/common/mean_transform.py
@@ -7,7 +7,7 @@ from autofeat.functional import mean_tf
 
 class MeanTransform(Transform):
     """
-    Compute the mean of the values in x where `where` is True.
+    Compute the mean of the values in `x` where `where` is True.
 
     Inherits from Transform.
     """
@@ -17,14 +17,14 @@ class MeanTransform(Transform):
     
     def __call__(self, signal_window: np.ndarray, where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]]=None) -> Union[np.float_, np.int_]:
         """
-        Apply the transformation to the signal window provided.
+        Compute the mean of the signal window provided.
 
         Args:
-            signal_window: The signal window to transform.
-            where: A function that takes a value and returns True or False. (Default: None)
+            `signal_window`: The signal window to find the mean of.
+            `where`: A function that takes a value and returns True or False. (Default: None)
         
         Returns:
-            A scalar value representing the transformation of the signal.
+            A scalar value representing the mean of the signal.
 
         """
         return mean_tf(signal_window, where=where)
