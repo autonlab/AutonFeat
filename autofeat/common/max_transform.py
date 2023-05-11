22993484976853bca2ec5512f1ee201833b9c30a
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-05-11 14:45:00 -0400

    Add max featurizer

---
+++ autofeat/common/max_transform.py
@@ -0,0 +1,30 @@
+import numpy as np
+from typing import Callable, Union, Any
+
+from autofeat.core import Transform
+from autofeat.functional import max_tf
+
+
+class MaxTransform(Transform):
+    """
+    Compute the max of the values in `x` where `where` is True.
+
+    Inherits from Transform.
+    """
+    # Dunder methods
+    def __init__(self):
+        super().__init__(name="Max")
+    
+    def __call__(self, signal_window: np.ndarray, where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]]=None) -> Union[np.float_, np.int_]:
+        """
+        Compute the max of the signal window provided.
+
+        Args:
+            `signal_window`: The signal window to find the max of.
+            `where`: A function that takes a value and returns True or False. (Default: None)
+        
+        Returns:
+            A scalar value representing the max of the signal.
+
+        """
+        return max_tf(signal_window, where=where)
