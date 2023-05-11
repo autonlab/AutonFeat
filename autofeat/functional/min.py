a1a21b6e582fec6518fcef8124f963c4bfad1806
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-05-11 15:03:51 -0400

    Update docstring formats to include markdown

--- autofeat/functional/min.py
+++ autofeat/functional/min.py
@@ -8,7 +8,7 @@ def min_tf(x: np.ndarray, where: Callable[[Union[int, float, np.int_, np.float_]
 
     Args:
         `x`: The array to compute the min of.
-        `where`: A function that takes a value and returns True or False. (Default: None)
+        `where`: A function that takes a value and returns `True` or `False`. Default is `None`.
 
     Returns:
         The min of the values in `x` where `where` is True.
