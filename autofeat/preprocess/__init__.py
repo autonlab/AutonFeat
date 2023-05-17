b0823d5635b70652b3147103bedf5303ce3e70f0
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-05-17 01:36:00 -0400

    Change distribution change functions to preprocess library

---
+++ autofeat/preprocess/__init__.py
@@ -0,0 +1,20 @@
+from .delta import delta_tf
+from .delta_max import delta_max_tf
+from .delta_min import delta_min_tf
+from .delta_mean import delta_mean_tf
+from .delta_median import delta_median_tf
+from .delta_std import delta_std_tf
+from .delta_var import delta_var_tf
+from .delta_quantile import delta_quantile_tf
+
+# For linter
+__all__ = [
+    "delta_tf",
+    "delta_max_tf",
+    "delta_min_tf",
+    "delta_mean_tf",
+    "delta_median_tf",
+    "delta_std_tf",
+    "delta_var_tf",
+    "delta_quantile_tf",
+]
