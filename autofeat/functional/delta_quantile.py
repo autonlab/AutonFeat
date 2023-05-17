e2d5bfd611eaccf5fee24511f9b00dbd5a2de8f3
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-05-17 00:12:53 -0400

    Add delta fn for quantile, std and var functional form featurizers

---
+++ autofeat/functional/delta_quantile.py
@@ -0,0 +1,24 @@
+import numpy as np
+from typing import Union, Callable
+from autofeat.functional import quantile_tf, delta_tf
+
+
+def delta_quantile_tf(x: np.ndarray, q: Union[float, np.float_], method: str = 'linear', where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]] = lambda x: not np.isnan(x)) -> Union[float, np.float_]:
+    """
+    Compute the difference between the values in `x` and `quantile` where `where` is `True`.
+
+    Args:
+        `x`: The array to compute the delta with.
+
+        `q`: The quantile to compute. Must be between 0 and 1.
+
+        `method`: The method to use when computing the quantile. Default is 'linear'. See `numpy.quantile` for more information.
+
+        `where`: A function that takes a value and returns `True` or `False`. Default is `lambda x: not np.isnan(x)` i.e. a measurement is valid if it is not a `NaN` value.
+
+    Returns:
+        The difference between the of the values in `x` and `quantile` where `where` is `True`.
+
+    """
+    quantile = quantile_tf(x, q=q, method=method, where=where)
+    return delta_tf(x, detla=quantile, where=where)
---
+++ autofeat/functional/delta_var.py
@@ -0,0 +1,22 @@
+import numpy as np
+from typing import Union, Callable
+from autofeat.functional import var_tf, delta_tf
+
+
+def delta_var_tf(x: np.ndarray, ddof: Union[int, np.int_] = 0, where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]] = lambda x: not np.isnan(x)) -> Union[float, np.float_]:
+    """
+    Compute the difference between the values in `x` and `var` where `where` is `True`.
+
+    Args:
+        `x`: The array to compute the delta with.
+
+        `ddof`: The delta degrees of freedom. Default is `0`. See `numpy.var` for more information.
+
+        `where`: A function that takes a value and returns `True` or `False`. Default is `lambda x: not np.isnan(x)` i.e. a measurement is valid if it is not a `NaN` value.
+
+    Returns:
+        The difference between the of the values in `x` and `var` where `where` is `True`.
+
+    """
+    var = var_tf(x, ddof=ddof, where=where)
+    return delta_tf(x, detla=var, where=where)
