d7f7f5d1d94c44767ba54c9a6a58811a95cb2aa5
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-05-23 14:30:44 -0400

    Add threading utility functions

---
+++ autofeat/utils/__init__.py
@@ -0,0 +1,6 @@
+from . import threads
+
+# For linter
+__all__ = [
+    'threads',
+]
---
+++ autofeat/utils/threads.py
@@ -0,0 +1,32 @@
+import numpy as np
+import numba as nb
+from typing import Union
+
+
+def check_num_threads(num_threads: Union[int, np.int_]) -> None:
+    """
+    Check if the number of threads is valid.
+
+    Args:
+        `num_threads`: The number of threads to use.
+
+    Raises:
+        `ValueError`: If `num_threads` is not a positive integer.
+
+        `ValueError`: If `num_threads` is greater than the number of available threads.
+    """
+    if not isinstance(num_threads, (int, np.int_)):
+        raise ValueError("num_threads must be an integer.")
+    if num_threads < 1:
+        raise ValueError("num_threads must be greater than 0.")
+    if num_threads > nb.get_num_threads():
+        raise ValueError("num_threads must be less than or equal to the number of available threads.")
+
+
+def set_num_threads(num_threads: Union[int, np.int_]) -> None:
+    """
+    Set the number of threads to use.
+    """
+    # Check if the number of threads is valid
+    check_num_threads(num_threads)
+    nb.set_num_threads(num_threads)
