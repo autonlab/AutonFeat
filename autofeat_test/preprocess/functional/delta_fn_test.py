2ec82215a98a0fc499a05e41da6cca375a581539
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-05-23 15:26:54 -0400

    Add functional form test for delta preprocessor with random values

---
+++ autofeat_test/preprocess/functional/delta_fn_test.py
@@ -0,0 +1,22 @@
+import numpy as np
+from autofeat.preprocess.transform import DeltaPreprocessor
+
+
+def test_delta_fn():
+    """
+    Test delta distribution shift preprocessor.
+    """
+    # Tolerance for numerical imprecision
+    tol = 1e-5
+
+    x = np.random.rand(100)
+
+    for _ in (10, 100, 10):
+        random_delta = np.random.rand()
+
+        tf = DeltaPreprocessor()
+        shifted_x_hat = tf(x, delta=random_delta)
+
+        shifted_x = x - random_delta
+
+        assert np.allclose(shifted_x_hat, shifted_x, atol=tol)
