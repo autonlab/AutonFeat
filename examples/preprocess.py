dee35af62179a188cf0d68decb67a038bdc51712
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-05-23 14:32:27 -0400

    Update examples for local package import

--- examples/preprocess.py
+++ examples/preprocess.py
@@ -1,5 +1,9 @@
-import autofeat as aft
 import numpy as np
+import sys
+package_path = '/Users/dhruvsrikanth/Work/CMU/AutoFeat'
+sys.path.append(package_path)
+
+import autofeat as aft
 
 
 def main():
@@ -14,9 +18,9 @@ def main():
     transformed_dist = preprocessor(dist)
 
     # Print features
-    print(preprocessor)
-    print(dist)
-    print(transformed_dist)
+    # print(preprocessor)
+    # print(dist)
+    print(transformed_dist.shape)
 
 
 if __name__ == '__main__':
--- examples/transform.py
+++ examples/transform.py
@@ -1,5 +1,9 @@
-import autofeat as aft
 import numpy as np
+import sys
+package_path = '/Users/dhruvsrikanth/Work/CMU/AutoFeat'
+sys.path.append(package_path)
+
+import autofeat as aft
 
 
 def main():
