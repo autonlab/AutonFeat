8336b32d2aecaf632959ba7e152feecfd20a9bcb
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-05-26 16:39:23 -0400

    Add sample entropy, cross entropy, shannon entropy and KL divergence featurizers

---
+++ autofeat/common/cross_entropy_transform.py
@@ -0,0 +1,34 @@
+import numpy as np
+from typing import Callable, Union
+
+from autofeat.core import Transform
+from autofeat.functional import cross_entropy_tf
+
+
+class CrossEntropyTransform(Transform):
+    """
+    Compute the skewness of the values in `x`.
+
+    Inherits from Transform.
+    """
+    # Dunder methods
+    def __init__(self, name: str = "Cross Entropy") -> None:
+        super().__init__(name=name)
+
+    def __call__(pk: np.ndarray, qk: np.ndarray = None, base: Union[int, np.int_] = None, where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]] = lambda x: not np.isnan(x)) -> Union[float, np.float_]:
+        """
+        Compute the cross-entropy of the values in `pk` with respect to `qk` where `where` is `True`.
+
+        Args:
+            `pk`: A discrete probability distribution.
+
+            `qk`: A second discrete probability distribution.
+
+            `base`: The base of the logarithm used to compute the entropy. Default is `None` which means that the natural logarithm is used.
+
+            `where`: A function that takes a value and returns `True` or `False`. Default is `lambda x: not np.isnan(x)` i.e. a measurement is valid if it is not a `NaN` value.
+
+        Returns:
+            The cross-entropy of the values in `pk` with respect to `qk` where `where` is `True`.
+        """
+        return cross_entropy_tf(pk, qk=qk, base=base, where=where)
---
+++ autofeat/common/entropy_transform.py
@@ -0,0 +1,37 @@
+import numpy as np
+from typing import Callable, Union
+
+from autofeat.core import Transform
+from autofeat.functional import entropy_tf
+
+
+class EntropyTransform(Transform):
+    """
+    Compute the entropy of a distribution, or the KL divergence between two distributions.
+
+    Inherits from Transform.
+    """
+    # Dunder methods
+    def __init__(self, name: str = "Shannon Entropy/KL Divergence") -> None:
+        super().__init__(name=name)
+
+    def __call__(pk: np.ndarray, qk: np.ndarray = None, base: Union[int, np.int_] = None, where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]] = lambda x: not np.isnan(x)) -> Union[float, np.float_]:
+        """
+        Compute the entropy of the values in `pk` where `where` is `True`.
+
+        Args:
+            `pk`: The discrete probability distribution to find the entropy of.
+
+            `qk`: The second discrete probability distribution to find the relative entropy with. Default is `None`.
+                            If `qk` is `None`, Shannon entropy is computed using `H = -sum(pk * log(pk))`.\n
+                            If `qk` is not `None`, relative entropy is computed using `H = -sum(pk * log(pk / qk))`. This is also called the Kullback-Leibler (KL) divergence.
+
+
+            `base`: The base of the logarithm used to compute the entropy. Default is `None` which means that the natural logarithm is used.
+
+            `where`: A function that takes a value and returns `True` or `False`. Default is `lambda x: not np.isnan(x)` i.e. a measurement is valid if it is not a `NaN` value.
+
+        Returns:
+            The entropy of the values in `pk` optionally with respect to `qk` (relative entropy) where `where` is `True`.
+        """
+        return entropy_tf(pk, qk=qk, base=base, where=where)
