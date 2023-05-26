9b46e47888c37189158563fbd2e019548ae9e613
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-05-26 17:19:13 -0400

    Correct fisher argument spelling

--- autofeat/common/kurtosis_transform.py
+++ autofeat/common/kurtosis_transform.py
@@ -15,7 +15,7 @@ class KurtosisTransform(Transform):
     def __init__(self, name: str = "Kurtosis") -> None:
         super().__init__(name=name)
 
-    def __call__(self, signal_window: np.ndarray, fischer: Union[bool, np.bool_], where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]] = lambda x: not np.isnan(x)) -> Union[np.float_, np.int_]:
+    def __call__(self, signal_window: np.ndarray, fisher: Union[bool, np.bool_], where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]] = lambda x: not np.isnan(x)) -> Union[np.float_, np.int_]:
         """
         Compute the krutosis of the values in `x` where `where` is `True`.\n
         The krutosis is a measure of the "tailedness" of a distribution. It is defined as the fourth standardized moment of a distribution, and is calculated as:
@@ -26,11 +26,11 @@ class KurtosisTransform(Transform):
         Args:
             `signal_window`: The signal to compute the krutosis of.
 
-            `fischer`: Whether to use Fisher's definition of kurtosis i.e. subtract 3 from the result. Default is `True`. If `False`, the result is the Pearson's definition of kurtosis.
+            `fisher`: Whether to use Fisher's definition of kurtosis i.e. subtract 3 from the result. Default is `True`. If `False`, the result is the Pearson's definition of kurtosis.
 
             `where`: A function that takes a value and returns `True` or `False`. Default is `lambda x: not np.isnan(x)` i.e. a measurement is valid if it is not a `NaN` value.
 
         Returns:
             The krutosis of the values in `x` where `where` is `True`.
         """
-        return kurtosis_tf(signal_window, fischer=fischer, where=where)
+        return kurtosis_tf(signal_window, fisher=fisher, where=where)
--- autofeat/functional/kurtosis.py
+++ autofeat/functional/kurtosis.py
@@ -2,7 +2,7 @@ import numpy as np
 from typing import Union, Callable
 
 
-def kurtosis_tf(x: np.ndarray, fischer: Union[bool, np.bool_] = True, where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]] = lambda x: not np.isnan(x)) -> Union[float, np.float_]:
+def kurtosis_tf(x: np.ndarray, fisher: Union[bool, np.bool_] = True, where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]] = lambda x: not np.isnan(x)) -> Union[float, np.float_]:
     """
     Compute the krutosis of the values in `x` where `where` is `True`.\n
     The krutosis is a measure of the "tailedness" of a distribution. It is defined as the fourth standardized moment of a distribution, and is calculated as:
@@ -13,7 +13,7 @@ def kurtosis_tf(x: np.ndarray, fischer: Union[bool, np.bool_] = True, where: Cal
     Args:
         `x`: The array to compute the krutosis of.
 
-        `fischer`: Whether to use Fisher's definition of kurtosis i.e. subtract 3 from the result. Default is `True`. If `False`, the result is the Pearson's definition of kurtosis.
+        `fisher`: Whether to use Fisher's definition of kurtosis i.e. subtract 3 from the result. Default is `True`. If `False`, the result is the Pearson's definition of kurtosis.
 
         `where`: A function that takes a value and returns `True` or `False`. Default is `lambda x: not np.isnan(x)` i.e. a measurement is valid if it is not a `NaN` value.
 
@@ -30,6 +30,6 @@ def kurtosis_tf(x: np.ndarray, fischer: Union[bool, np.bool_] = True, where: Cal
     kurtosis = np.mean(((filtered_x - sample_mean) / sample_std) ** 4)
 
     # Fisher's definition
-    kurtosis = kurtosis - 3.0 if fischer else kurtosis
+    kurtosis = kurtosis - 3.0 if fisher else kurtosis
 
     return kurtosis
