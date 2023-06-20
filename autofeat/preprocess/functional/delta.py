860e56e9b4893d2dbb7252f08b674318f8e756e9
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-06-19 20:33:09 -0400

    [DOCS] Modify docstrings for mkdocs

--- autofeat/preprocess/functional/delta.py
+++ autofeat/preprocess/functional/delta.py
@@ -4,17 +4,17 @@ from typing import Union, Callable
 
 def delta_tf(x: np.ndarray, delta: Union[int, float, np.int_, np.float_], where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]] = lambda x: not np.isnan(x)) -> np.ndarray:
     """
-    Compute the difference between the values in `x` and `delta` where `where` is `True`.
+    Preprocess the signal by shifting the `signal` by some `delta` value.
 
     Args:
-        `x`: The array to compute the delta with.
+        x: The signal to preprocess.
 
-        `delta`: The value to compute the delta with.
+        delta: The value to shift the signal by.
 
-        `where`: A function that takes a value and returns `True` or `False`. Default is `lambda x: not np.isnan(x)` i.e. a measurement is valid if it is not a `NaN` value.
+        where: A function that takes a value and returns `True` or `False`. Default is `lambda x: not np.isnan(x)` i.e. a measurement is valid if it is not a `NaN` value.
 
     Returns:
-        The difference between the of the values in `x` and `delta` where `where` is `True`.
+        The shifted signal.
     """
     # Vectorize where fn
     where_fn = np.vectorize(where)
--- autofeat/preprocess/functional/dft.py
+++ autofeat/preprocess/functional/dft.py
@@ -7,17 +7,17 @@ def dft_tf(x: np.ndarray, n: Union[int, np.int_] = None, norm: str = 'backward',
     Compute the 1D discete Fourier Transform (DFT) using Fast-Fourier Transform (FFT) on the values in `x`.
 
     Args:
-        `x`: The array to compute the DFT of.
+        x: The array to compute the DFT of.
 
-        `n`: The number of points to use for the FFT. If `None`, the length of `x` is used. Default is `None`.
+        n: The number of points to use for the FFT. If `None`, the length of `x` is used. Default is `None`.
 
-        `norm`: The normalization mode to use. Default is 'backward'. See `numpy.fft` for more information.
-                    \tOptions include:\n
-                    \t\t`'backward'`: The backward transform is scaled by `1/n`.\n
-                    \t\t`'ortho'`: The forward and backward transforms are scaled by `1/sqrt(n)`.\n
-                    \t\t`'forward'`: The forward transform is not scaled.\n
+        norm: The normalization mode to use. Default is 'backward'. See `numpy.fft` for more information.
+                    Options include:\n
+                    'backward': The backward transform is scaled by `1/n`.
+                    'ortho': The forward and backward transforms are scaled by `1/sqrt(n)`.
+                    'forward': The forward transform is not scaled.
 
-        `where`: A function that takes a value and returns `True` or `False`. Default is `lambda x: not np.isnan(x)` i.e. a measurement is valid if it is not a `NaN` value.
+        where: A function that takes a value and returns `True` or `False`. Default is `lambda x: not np.isnan(x)` i.e. a measurement is valid if it is not a `NaN` value.
 
     Returns:
         The 1D DFT of `x`.
