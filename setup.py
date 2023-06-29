356159481eb18ca887c6de9386fb6fc00feb4889
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-06-29 14:31:45 -0400

    [PROJ] Update install reqs

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ setup(
     name=setup_properties.get_name(),
     version=setup_properties.get_version(),
     description=setup_properties.get_description(),
-    author=setup_properties.get_author(),
+    author=[a for a in setup_properties.get_author()],
     author_email=setup_properties.get_author_email(),
     url=setup_properties.get_url(),
-    packages=[p for p in setup_properties.get_packages()],
+    install_requires=[p for p in setup_properties.get_packages()],
+    license=setup_properties.get_license(),
 )
