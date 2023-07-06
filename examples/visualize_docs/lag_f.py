a7deb5b040f003eb9ed99c8728159de751559827
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-07-05 22:47:11 -0400

    [DOCS] Update single feature extraction to use lag preprocessor

---
+++ examples/visualize_docs/lag_f.py
@@ -0,0 +1,39 @@
+import sys
+package_path = '/Users/dhruvsrikanth/Work/CMU/AutoFeat'
+sys.path.append(package_path)
+
+import numpy as np
+import matplotlib.pyplot as plt
+import autofeat.preprocess.functional as PF
+
+
+def main():
+
+    # Define signal
+    num_samples = 10
+    signal = np.arange(1, num_samples + 1)
+
+    lag = 2
+
+    # Preprocess and transform signal
+    transformed_signal = PF.lag_tf(signal, lag=lag)
+    transformed_signal = np.nan_to_num(transformed_signal)
+
+    # Plot results
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5))
+    ax.plot(signal, label='Original Signal')
+    ax.plot(transformed_signal, label='Lag Transformed Signal')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Signal')
+    ax.set_title('Lag Preprocessor')
+    ax.legend()
+    ax.grid()
+
+    plt.tight_layout()
+
+    # Save figure
+    fig.savefig('../../docs/assets/lag_f_visualize.png')
+
+
+if __name__ == '__main__':
+    main()
