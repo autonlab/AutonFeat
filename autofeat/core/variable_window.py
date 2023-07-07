8196db44ec62ae694d76e32298751f45ca7effac
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-07-07 17:06:24 -0400

    [PROJ] Rename package to meet pypi open names

--- autofeat/core/variable_window.py
+++
@@ -1,7 +0,0 @@
-# import numpy as np
-# import numba as nb
-# from typing import Union, Callable, List
-
-
-# class VariableWindow(object):
-#     pass
--- autofeat/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import threads
-import autofeat.utils.datasets as datasets
-
-# For linter
-__all__ = [
-    'threads',
-    'datasets',
-]
