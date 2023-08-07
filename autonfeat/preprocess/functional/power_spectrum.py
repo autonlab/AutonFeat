ef7f297e283a1e21b9534489d38c8481ca6c0944
Dhruv Srikanth <dhruvsrikanth5@gmail.com>
2023-08-07 12:44:13 -0500

    [FEAT] Update arg refs in power spectrum to reflect use of fft

--- autonfeat/preprocess/functional/power_spectrum.py
+++ autonfeat/preprocess/functional/power_spectrum.py
@@ -25,16 +25,16 @@ from typing import Union, Callable
 from autonfeat.preprocess.functional.dft import dft_tf
 
 
-def power_spectrum_tf(x: np.ndarray, n: Union[int, np.int_] = None, norm: str = 'backward', where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]] = lambda x: not np.isnan(x)) -> Union[float, np.float_]:
+def power_spectrum_tf(x: np.ndarray, nfft: Union[int, np.int_] = None, normfft: str = 'backward', where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]] = lambda x: not np.isnan(x)) -> Union[float, np.float_]:
     """
     Compute the power spectrum on the values in `x`. This uses a 1D DFT to compute the power spectrum. See `autonfeat.preprocess.functional.dft` for more information.
 
     Args:
         x: The array to compute the power spectrum of.
 
-        n: The number of points to use for the FFT (1D DFT). If `None`, the length of `x` is used. Default is `None`.
+        nfft: The number of points to use for the FFT (1D DFT). If `None`, the length of `x` is used. Default is `None`.
 
-        norm: The normalization mode to use. Default is 'backward'. See `autonfeat.preprocess.functional.dft` for more information.
+        normfft: The normalization mode to use when computng the FFT (1D DFT). Default is 'backward'. See `autonfeat.preprocess.functional.dft` for more information.
                     Options include:\n
                     'backward': The backward transform is scaled by `1/n`.
                     'ortho': The forward and backward transforms are scaled by `1/sqrt(n)`.
@@ -50,7 +50,7 @@ def power_spectrum_tf(x: np.ndarray, n: Union[int, np.int_] = None, norm: str = 
     filtered_x = x[where_fn(x)]
 
     # Compute the DFT
-    dft = dft_tf(x=filtered_x, n=n, norm=norm, where=where)
+    dft = dft_tf(x=filtered_x, n=nfft, norm=normfft, where=where)
 
     # Compute the power spectrum
     return np.abs(dft)**2
--- autonfeat/preprocess/transform/power_spectrum_processor.py
+++ autonfeat/preprocess/transform/power_spectrum_processor.py
@@ -34,16 +34,16 @@ class PowerSpectrumPreprocessor(Preprocess):
     def __init__(self, name: str = "Power Spectrumm") -> None:
         super().__init__(name=name)
 
-    def __call__(self, signal: np.ndarray, n: Union[int, np.int_] = None, norm: str = 'backward', where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]] = lambda x: not np.isnan(x)) -> np.ndarray:
+    def __call__(self, signal: np.ndarray, nfft: Union[int, np.int_] = None, normfft: str = 'backward', where: Callable[[Union[int, float, np.int_, np.float_]], Union[bool, np.bool_]] = lambda x: not np.isnan(x)) -> np.ndarray:
         """
         Compute the power spectrum on the values in `x`. This uses a 1D DFT to compute the power spectrum. See `autonfeat.preprocess.functional.dft` for more information.
 
         Args:
             signal: The array to compute the power spectrum of.
 
-            n: The number of points to use for the FFT (1D DFT). If `None`, the length of `x` is used. Default is `None`.
+            nfft: The number of points to use for the FFT (1D DFT). If `None`, the length of `x` is used. Default is `None`.
 
-            norm: The normalization mode to use. Default is 'backward'. See `autonfeat.preprocess.functional.dft` for more information.
+            normfft: The normalization mode to use when computng the FFT (1D DFT). Default is 'backward'. See `autonfeat.preprocess.functional.dft` for more information.
                     Options include:\n
                     'backward': The backward transform is scaled by `1/n`.
                     'ortho': The forward and backward transforms are scaled by `1/sqrt(n)`.
@@ -54,4 +54,4 @@ class PowerSpectrumPreprocessor(Preprocess):
         Returns:
             The power spectrum of `x`.
         """
-        return power_spectrum_tf(x=signal, n=n, norm=norm, where=where)
+        return power_spectrum_tf(x=signal, nfft=nfft, normfft=normfft, where=where)
